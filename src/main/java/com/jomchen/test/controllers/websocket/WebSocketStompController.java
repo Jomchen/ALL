62d16631518ea234ef3be7bebdc43b1a0629bf5a
Jomchen <937574740@qq.com>
2018-12-31 23:05:55 +0800

    增加了几个请求方法，指定个人方法未解决

--- src/main/java/com/jomchen/test/controllers/websocket/WebSocketStompController.java
+++ src/main/java/com/jomchen/test/controllers/websocket/WebSocketStompController.java
@@ -1,12 +1,17 @@
 package com.jomchen.test.controllers.websocket;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jomchen.test.utils.RequestIdGenerator;
 import com.jomchen.test.utils.RequestObject;
 import com.jomchen.test.utils.ResultObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +24,16 @@ public class WebSocketStompController {
 
     private Logger logger = LoggerFactory.getLogger(WebSocketStompController.class);
 
-    @MessageMapping("/greeting") // /app/greenting 可以请求到
+    @Autowired
+    @Qualifier("brokerMessagingTemplate")
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    /**
+     * 广播信息
+     * /app/greenting 可以请求到这里
+     * 订阅 /topic/greeting
+     */
+    @MessageMapping("/greeting")
     public ResultObject stompGreeting(@RequestBody RequestObject<String> requestObject) {
         logger.info("This is content of Stomp =>" + requestObject);
         ResultObject<String> resultObject = ResultObject.buildSuccess(
@@ -29,6 +43,40 @@ public class WebSocketStompController {
         return resultObject;
     }
 
+    /**
+     * 这里是发给了自己
+     * /app/stompToUser 可以请求到这里
+     * 订阅 /user/queue/sendToMySelf 可以接到指定发送
+     */
+    @MessageMapping("/sendToMySelf")
+    @SendToUser("/queue/sendToMySelf")
+    public ResultObject sendToMySelf(@RequestBody RequestObject<String> requestObject) {
+        ResultObject<String> resultObject = ResultObject.buildSuccess(
+                RequestIdGenerator.generate(),
+                requestObject.getData()
+        );
+
+        return resultObject;
+    }
+
+    /**
+     * 发给指定用户
+     * /app/sendToUser 可以请求到这里
+     * 订阅 /user/queue/stompToUser 相应用户可以接收到这里反馈的信息
+     */
+    @MessageMapping("/sendToUser")
+    public void sendToUser(@RequestBody RequestObject<String> requestObject) {
+        String jsonStr = requestObject.getData();
+        JSONObject jsonObject = JSONObject.parseObject(jsonStr);
+        String sendUser = jsonObject.getString("sendUser");
+        String sendData = jsonObject.getString("sendData");
+        simpMessagingTemplate.convertAndSendToUser(sendUser, "/queue/sendToUser", sendData);
+    }
+
+    /**
+     * 一次性订阅
+     * 订阅 /app//once_once 可以一次性得到信息，这里相当于在初始化
+     */
     @SubscribeMapping("/once_once") // /app/once_once 可以一次性订阅到
     public ResultObject subscribe() {
         ResultObject<String> resultObject = ResultObject.buildSuccess(
@@ -38,6 +86,9 @@ public class WebSocketStompController {
         return resultObject;
     }
 
+    /**
+     * 处理错误
+     */
     @MessageExceptionHandler
     public ResultObject handleError(Throwable throwable) {
         logger.error(throwable.getMessage(), throwable);
--- src/main/resource/template/static/js/websocket/stomp_page.js
+++ src/main/resource/template/static/js/websocket/stomp_page.js
@@ -6,15 +6,17 @@ $(function () {
      * 建立连接
      */
     $("#setUpConnection").click(function () {
+        var loginName = prompt("请办理入您的登录名字，默认为 jomchen", "jomchen");
+
         var sockJS = new SockJS('http://127.0.0.1:8080/stompPoint');
         stompClient = Stomp.over(sockJS);
         stompClient.connect(
             /*{"login": null, "passcode": null // 其它回调},*/
-            {"username": "jomchen", "password": "password"},
+            {"username": loginName, "password": loginName},
             function() {
                 $("#receiveData").append("<p>已经建立连接</p>");
                 // 订阅的第三个参数是在发订阅帧时指定的id，如果未指定会自动生成 {id: "xxx"}
-                var subscribeIdOne = this.subscribe("/topic/greeting", function(data) {
+                var broadId = this.subscribe("/topic/greeting", function(data) {
                     console.log("topic接收到参数是：" + data);
                     var resultData = JSON.parse(data.body);
                     if (resultData.code == 0) {
@@ -23,10 +25,20 @@ $(function () {
                         $("#receiveData").append("<p>topic回调体=>" + resultData.message + "</p>");
                     }
                 }, {"otherHeaders": "otherHeaders"});
-                var subscribeIdTwo = this.subscribe("/queue/greeting", function(data) {
+                var queueId = this.subscribe("/queue/greeting", function(data) {
                     console.log("queue接收到参数是：" + data);
                     var resultData = JSON.parse(data.body);
                     $("#receiveData").append("<p>queue回调体=>" + resultData.data + "</p>");
+                }, {"otherHeaders": "otherHeaders"});
+                var sendToMySelfId = this.subscribe("/user/queue/sendToMySelf", function(data) {
+                    console.log("发给自己的接收到参数是：" + data);
+                    var resultData = JSON.parse(data.body);
+                    $("#receiveData").append("<p>发给自己的回调体=>" + resultData.data + "</p>");
+                }, {"otherHeaders": "otherHeaders"});
+                var sendToUserId = this.subscribe("/user/queue/sendToUser", function(data) {
+                    console.log("指定发送，接收到参数是：" + data);
+                    var resultData = JSON.parse(data.body);
+                    $("#receiveData").append("<p>指定发送的回调体=>" + resultData.data + "</p>");
                 }, {"otherHeaders": "otherHeaders"});
                 this.subscribe("/app/once_once", function(data) {
                     var resultData = JSON.parse(data.body);
@@ -50,8 +62,23 @@ $(function () {
         var sendData = $("#sendData").val();
         var sendJson = {"requestId": "Linux", "data": sendData};
         var sendStomp = JSON.stringify(sendJson);
-        stompClient.send("/app/greeting", {}, sendStomp);
 
+        var hasBroad = confirm("是否广播发送？");
+        if (hasBroad) {
+            stompClient.send("/app/greeting", {}, sendStomp);
+            return;
+        }
+        var hasPersonal = confirm("是否发给自己？");
+        if (hasPersonal) {
+            stompClient.send("/app/sendToMySelf", {}, sendStomp);
+            return;
+        }
+
+        var sendUser = prompt("指定您要发送的用户，默认为 jomchen", "jomchen");
+        var sendData = prompt("指定要发送的信息，默认为 LINUX", "LINUX");
+        var sendDataObj = {"sendUser": sendUser, "sendData": sendData};
+        sendJson['data'] = sendDataObj;
+        stompClient.send("/app/sendToUser", {}, JSON.stringify(sendJson));
     });
 
     /**
