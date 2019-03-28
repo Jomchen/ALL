49f6e262785175fd40407accd72c9211a8f4c3be
Jomchen <937574740@qq.com>
2019-03-28 22:11:52 +0800

    增加了一个下载的功能

--- src/main/java/com/jomchen/test/controllers/IndexController.java
+++ src/main/java/com/jomchen/test/controllers/IndexController.java
@@ -2,10 +2,15 @@ package com.jomchen.test.controllers;
 
 import com.jomchen.test.utils.UrlContents;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,6 +24,9 @@ public class IndexController {
     @Autowired
     private HttpServletRequest request;
 
+    @Autowired
+    private HttpServletResponse response;
+
     @RequestMapping(value = UrlContents.INDEX)
     public String index() {
         List<String> list = new ArrayList<>();
@@ -29,6 +37,33 @@ public class IndexController {
         request.setAttribute("list", list);
         request.setAttribute("username", "李寻欢");
         return "index";
+    }
+
+    @RequestMapping(value = "/download")
+    public void download() throws IOException {
+        FileSystemResource fileSystemSource = new FileSystemResource("/opt/ngrok.config.tar.gz");
+        String fileName = fileSystemSource.getFilename();
+        InputStream inputStream = fileSystemSource.getInputStream();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] bytes = new byte[1024];
+        int i;
+        while ( (i = inputStream.read(bytes)) != -1 ) {
+            byteArrayOutputStream.write(bytes, 0, i);
+        }
+        inputStream.close();
+
+
+        // 配置文件下载
+        response.setHeader("content-type", "application/octet-stream");
+        response.setContentType("application/octet-stream");
+        // 下载文件能正常显示中文
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+        byte[] data = byteArrayOutputStream.toByteArray();
+        OutputStream outputStream = response.getOutputStream();
+        outputStream.write(data);
+        outputStream.flush();
+        outputStream.close();
     }
 
 }
