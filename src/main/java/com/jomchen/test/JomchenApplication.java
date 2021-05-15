d31e74c2ac4d1ccca89075237e72e2bc7971c3e8
Jomkie <937574740@qq.com>
2021-05-15 11:22:25 +0800

    修改

--- src/main/java/com/jomchen/test/JomchenApplication.java
+++ src/main/java/com/jomchen/test/JomchenApplication.java
@@ -3,14 +3,36 @@ package com.jomchen.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
+
 /**
  * create by Jomchen on 2018/11/19
  */
-@SpringBootApplication
 public class JomchenApplication {
 
+    public static AtomicBoolean atomicBoolean = new AtomicBoolean(Boolean.TRUE);
+
     public static void main(String[] args) {
-        SpringApplication.run(JomchenApplication.class, args);
+        new Thread(new MyThread()).start();
+        IntStream.rangeClosed(0, 20).forEach(i -> {
+            boolean b = JomchenApplication.atomicBoolean.compareAndSet(Boolean.TRUE, Boolean.FALSE);
+            System.out.println("main ------------> " + i + "--" + b);
+        });
+    }
+
+}
+
+class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+        IntStream.rangeClosed(0, 21).forEach(k -> {
+            boolean b = JomchenApplication.atomicBoolean.compareAndSet(Boolean.FALSE, Boolean.TRUE);
+            System.out.println("son ***** " + k + "**" + b);
+
+        });
     }
 
 }
